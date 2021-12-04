25522320a5f5e91977e5bcd58b42a051436fb490
Thomas Theunen <thomas.theunen@forward.eu>
2021-12-04 11:27:58 +0100

    Add unit tests for fetching an entire object

--- test/unit/lib/cli-api/attributegroup-get.test.js
+++ test/unit/lib/cli-api/attributegroup-get.test.js
@@ -13,13 +13,15 @@ let dummyObjects = {
     }]
 };
 
-let dummyAttributeGroups = [
-    {
-        id: 'group1',
-    }, {
-        id: 'group2',
-    }
-]
+let dummyAttributeGroups = {
+    data: [
+        {
+            id: 'group1',
+        }, {
+            id: 'group2',
+        }
+    ]
+};
 
 let dummyAttributeGroup = {
     id: 'group1',
@@ -140,6 +142,28 @@ describe('attributegroup-get', () => {
             expect(outputCommandBookEndSpy.calledTwice).to.be.true;
         });
 
+        it('should not save files if the doNotSave flag is set', async () => {
+            await attributeGroupGet({
+                object: 'object',
+                attributeGroup: 'group1',
+                doNotSave: true
+            })
+
+            expect(writeFileSpy.notCalled).to.be.true;
+
+            // Output
+            expect(outputFieldsSpy.called).to.be.false;
+            expect(outputSuccessSpy.calledOnce).to.be.true;
+            expect(outputSuccessSpy.firstCall.args[0]).to.contain('group1');
+
+            expect(outputCommandBookEndSpy.calledTwice).to.be.true;
+
+            // Should call the OCAPI
+            expect(getAttributeGroupForObjectStub.calledOnce).to.be.true;
+            expect(getAttributeGroupsForObjectStub.called).to.be.false;
+            expect(getObjectTypesOCAPICallStub.called).to.be.false;
+        });
+
         it('should output an error if we get an error response from the OCAPI', async () => {
             sinon.restore();
 
@@ -162,6 +186,132 @@ describe('attributegroup-get', () => {
             // Should call the OCAPI
             expect(getAttributeGroupForObjectStub.calledOnce).to.be.true;
             expect(getAttributeGroupsForObjectStub.called).to.be.false;
+            expect(getObjectTypesOCAPICallStub.called).to.be.false;
+        });
+    });
+
+    describe('All Attribute Groups of a single object', () => {
+        it('should save all Attribute Groups for an object when Object is passed', async () => {
+            await attributeGroupGet({
+                object: 'object'
+            })
+
+            let resultGroupWithoutDefinitions = {...dummyAttributeGroup}
+            delete resultGroupWithoutDefinitions.attribute_definitions;
+
+            expect(writeFileSpy.callCount).to.equal(4);
+
+            // First group
+            expect(writeFileSpy.firstCall.args[0]).to.contain(path.join('object', 'groups', 'group1.json'));
+            expect(writeFileSpy.firstCall.args[1]).to.equal(JSON.stringify(cleanOCAPIResponse(resultGroupWithoutDefinitions), null, 4));
+            expect(writeFileSpy.secondCall.args[0]).to.contain(path.join('object', 'groups', 'group1-assignments.json'));
+            expect(writeFileSpy.secondCall.args[1]).to.equal(JSON.stringify(['attribute1', 'attribute2'], null, 4));
+
+            // Second group
+            expect(writeFileSpy.thirdCall.args[0]).to.contain(path.join('object', 'groups', 'group2.json'));
+            expect(writeFileSpy.thirdCall.args[1]).to.equal(JSON.stringify(cleanOCAPIResponse(resultGroupWithoutDefinitions), null, 4));
+            expect(writeFileSpy.getCall(3).args[0]).to.contain(path.join('object', 'groups', 'group2-assignments.json'));
+            expect(writeFileSpy.getCall(3).args[1]).to.equal(JSON.stringify(['attribute1', 'attribute2'], null, 4));
+
+            // Output
+            expect(outputFieldsSpy.called).to.be.false;
+            expect(outputSuccessSpy.calledTwice).to.be.true;
+            expect(outputSuccessSpy.firstCall.args[0]).to.contain('group1');
+            expect(outputSuccessSpy.secondCall.args[0]).to.contain('group2');
+
+            expect(outputCommandBookEndSpy.calledTwice).to.be.true;
+
+            // Should call the OCAPI
+            expect(getAttributeGroupForObjectStub.callCount).to.equal(4);
+            expect(getAttributeGroupsForObjectStub.calledOnce).to.be.true;
+            expect(getObjectTypesOCAPICallStub.called).to.be.false;
+        });
+
+        it('should output an error if we get an error response from the OCAPI when fetching all Attribute Groups', async () => {
+            sinon.restore();
+
+            getAttributeGroupsForObjectStub = sinon.stub(SystemObjectDefinition.prototype, 'getAttributeGroupsForObject').resolves({
+                isSuccess: () => false,
+                getFaultMessage: () => 'Test Error'
+            });
+
+            await attributeGroupGet({
+                object: 'object'
+            })
+
+            // Output
+            expect(outputFieldsSpy.called).to.be.false;
+            expect(outputSuccessSpy.called).to.be.false;
+            expect(outputErrorSpy.calledOnce).to.be.true;
+            expect(outputErrorSpy.firstCall.args[0]).to.contain('Test Error');
+
+            // Should call the OCAPI
+            expect(getAttributeGroupsForObjectStub.calledOnce).to.be.true;
+            expect(getAttributeGroupForObjectStub.called).to.be.false;
+            expect(getObjectTypesOCAPICallStub.called).to.be.false;
+        });
+
+        it('should output an error if we get an error response from the OCAPI when fetching an Attribute Group', async () => {
+            sinon.restore();
+
+            getAttributeGroupsForObjectStub = sinon.stub(SystemObjectDefinition.prototype, 'getAttributeGroupsForObject').resolves({
+                isSuccess: () => true,
+                data: dummyAttributeGroups
+            });
+
+            getAttributeGroupForObjectStub = sinon.stub(SystemObjectDefinition.prototype, 'getAttributeGroupForObject').resolves({
+                isSuccess: () => false,
+                getFaultMessage: () => 'Test Error'
+            });
+
+            await attributeGroupGet({
+                object: 'object'
+            })
+
+            // Output
+            expect(outputFieldsSpy.called).to.be.false;
+            expect(outputSuccessSpy.called).to.be.false;
+            expect(outputErrorSpy.calledTwice).to.be.true;
+            expect(outputErrorSpy.firstCall.args[0]).to.contain('Test Error');
+            expect(outputErrorSpy.secondCall.args[0]).to.contain('Test Error');
+
+            // Should call the OCAPI
+            expect(getAttributeGroupsForObjectStub.calledOnce).to.be.true;
+            expect(getAttributeGroupForObjectStub.calledTwice).to.be.true;
+            expect(getObjectTypesOCAPICallStub.called).to.be.false;
+        });
+
+        it('should output debug information if the debug flag is set', async () => {
+            await attributeGroupGet({
+                object: 'object',
+                debug: true
+            })
+
+            // Output
+            expect(outputFieldsSpy.calledTwice).to.be.true;
+            expect(outputSuccessSpy.calledTwice).to.be.true;
+            expect(outputCommandBookEndSpy.calledTwice).to.be.true;
+        });
+
+        it('should not save files if the doNotSave flag is set', async () => {
+            await attributeGroupGet({
+                object: 'object',
+                doNotSave: true
+            })
+
+            expect(writeFileSpy.notCalled).to.be.true;
+
+            // Output
+            expect(outputFieldsSpy.called).to.be.false;
+            expect(outputSuccessSpy.calledTwice).to.be.true;
+            expect(outputSuccessSpy.firstCall.args[0]).to.contain('group1');
+            expect(outputSuccessSpy.secondCall.args[0]).to.contain('group2');
+
+            expect(outputCommandBookEndSpy.calledTwice).to.be.true;
+
+            // Should call the OCAPI
+            expect(getAttributeGroupForObjectStub.calledTwice).to.be.true;
+            expect(getAttributeGroupsForObjectStub.calledOnce).to.be.true;
             expect(getObjectTypesOCAPICallStub.called).to.be.false;
         });
     });
